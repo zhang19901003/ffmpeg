851f394f35dd29a88ec1707c7073f5ad95149d2f
zyp <zyp19901003@163.com>
2018-10-08 17:41:28 +0800

    Temp commit

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -10,12 +10,9 @@
 #define LOGF(...)__android_log_print(ANDROID_LOG_FATAL,TAG,__VA_ARGS__)//定义LOGF类型
 
 extern "C" {
-//编码
 #include "libavcodec/avcodec.h"
-//封装格式处理
 #include "libavformat/avformat.h"
 #include "libswresample/swresample.h"
-//像素处理
 #include "libswscale/swscale.h"
 #include <android/native_window_jni.h>
 #include <unistd.h>
@@ -31,7 +28,64 @@ Java_com_adasplus_update_c_MainActivity_stringFromJNI(JNIEnv *env, jobject  ) {
     char info[10000] = { 0 };
     sprintf(info, "%s\n", avcodec_configuration());
     LOGE("%s",avcodec_configuration());
-    return env->NewStringUTF(info);
+
+    av_register_all();
+    char path[1024] = "/sdcard/aa.mp4";
+    AVFormatContext *ic = NULL;
+    int videoStream = 0;
+    int re = avformat_open_input(&ic, path, 0, 0);
+    if (re != 0)
+    {
+        char errorbuf[1024] = { 0 };
+        av_strerror(re, errorbuf, sizeof(errorbuf));
+        LOGE("%s","open failure");
+        return env->NewStringUTF(info);
+    }
+
+    for (int i = 0; i < ic->nb_streams; i++)
+    {
+        AVCodecContext *enc = ic->streams[i]->codec;
+        if (enc->codec_type == AVMEDIA_TYPE_VIDEO)
+        {
+            videoStream = i;
+            LOGE("%d",videoStream);
+            LOGE("%d",ic->nb_streams);
+            AVCodec *codec = avcodec_find_decoder(enc->codec_id);
+            if (!codec)
+            {
+                printf("video code not find!\n");
+                return env->NewStringUTF(info);
+            }
+            int err = avcodec_open2(enc, codec, NULL);
+            if (err != 0)
+
+            {
+                char buf[1024] = { 0 };
+                av_strerror(err, buf, sizeof(buf));
+                return env->NewStringUTF(info);
+            }
+        }
+    }
+
+
+
+
+
+    AVPacket pkt;
+    memset(&pkt, 0, sizeof(AVPacket));
+    for (;;) {
+
+        int err = av_read_frame(ic, &pkt);
+        if (err != 0) {
+            char buf[1024] = {0};
+            av_strerror(err, buf, sizeof(buf));
+            break;
+        }
+        if (pkt.stream_index == videoStream) {
+            LOGE("%d", pkt.size);
+        }
+        av_packet_unref(&pkt);
+    }
 }
 
 
