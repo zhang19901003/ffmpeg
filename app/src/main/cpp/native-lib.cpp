551b9e70a2425bd6b7ad230df1d3f52cefb43dff
zyp <zyp19901003@163.com>
2018-10-08 17:13:54 +0800

    Temp commit

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,14 +1,6 @@
 #include <jni.h>
 #include <string>
-#include "iostream"
-#include <stdio.h>
-#include <iostream>
-#include <strstream>
-#include <string.h>
-#include <stdlib.h>
-#include "android/log.h"
-using namespace std;
-
+#include <android/log.h>
 #define LOG_TAG "native-dev"#define LOGI(...)
 #define TAG"Adas"
 #define LOGD(...)__android_log_print(ANDROID_LOG_DEBUG,TAG,__VA_ARGS__)//定义LOGD类型
@@ -17,78 +9,30 @@ using namespace std;
 #define LOGE(...)__android_log_print(ANDROID_LOG_ERROR,TAG,__VA_ARGS__)//定义LOGE类型
 #define LOGF(...)__android_log_print(ANDROID_LOG_FATAL,TAG,__VA_ARGS__)//定义LOGF类型
 
+extern "C" {
+//编码
+#include "libavcodec/avcodec.h"
+//封装格式处理
+#include "libavformat/avformat.h"
+#include "libswresample/swresample.h"
+//像素处理
+#include "libswscale/swscale.h"
+#include <android/native_window_jni.h>
+#include <unistd.h>
+#include <SLES/OpenSLES.h>
+#include <SLES/OpenSLES_Android.h>
+}
+
+
 
 extern "C"
 JNIEXPORT jstring JNICALL
-Java_com_adasplus_update_c_MainActivity_stringFromJNI(
-        JNIEnv *env,
-        jobject /* this */) {
-    string hello = "Hello from C++aa";
-    typedef struct Student {
-        int age;
-        string name;
-
-        int age1;
-
-    } Student;
-//    char *name = "helloworld";
-//  //  LOGE("%d",strlen(hello.c_str()));
-//    char *otherName = "aaaa";
-//
-//    LOGE("%d",sizeof(name));
-//    LOGE("%d",sizeof(otherName));
-//    LOGE("%d", sizeof(Student));
-//
-//    Student *s = (Student *) malloc(sizeof(Student));
-//    LOGE("%d", sizeof(s));
-//    LOGE("%d", sizeof(int)*10);
-//
-//    int *p1 = (int*)malloc(sizeof(int)*2);
-//    int *p2 = (int*)malloc(sizeof(int)*2);
-//    LOGE("p1%p ",  p1);
-//    LOGE("p2%p ",  p2);
-//
-//    char *p3 = (char *)malloc(4);
-//    char *p4 = (char *)malloc(sizeof(char)*4);
-//    LOGE("p3%p ",  p3);
-//    LOGE("p4%p ",  p4);
-
-    int a11;
-
-    int c11[] = {1,2,3,4,5};
-    LOGE("a11 point%p ", &a11);
-
-    LOGE("b11 point %p ",c11);
-
-
-//    char d[] = {'x', 'y', 'z', 'q', 'g', 'f', 'x', 'y', 'z', 'q', 'g', 'f', 'x', 'y', 'z', 'q', 'g',
-//                'f'};
-//    char c[] = {'a'};
-//    LOGE("d point%p ", d);
-//    LOGE("c point %p ", c);
-
-
-
-
-
-
-
-
-
-
-
-    //   strcpy(name,otherName);
-//    Student *s = new Student;
-//    s->age = 10;
-//    s->name = "asd";
-//    strstream ss;
-//    string s1;
-//    ss << 10;
-//    ss >> s1;
-//    cout << s << endl;
-//    cout << s->name << s->name << endl;
-    //   hello += name;
-
-
-    return env->NewStringUTF(hello.c_str());
+Java_com_adasplus_update_c_MainActivity_stringFromJNI(JNIEnv *env, jobject  ) {
+    char info[10000] = { 0 };
+    sprintf(info, "%s\n", avcodec_configuration());
+    LOGE("%s",avcodec_configuration());
+    return env->NewStringUTF(info);
 }
+
+
+
--- app/src/main/java/com/adasplus/update/c/MainActivity.java
+++ app/src/main/java/com/adasplus/update/c/MainActivity.java
@@ -1,30 +1,39 @@
 package com.adasplus.update.c;
 
+import android.Manifest;
 import android.app.Activity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
-    // Used to load the 'native-lib' library on application startup.
     static {
-        System.loadLibrary("native-lib");
-    }
+              System.loadLibrary("native-lib");
+           }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Example of a call to a native method
         TextView tv = (TextView) findViewById(R.id.sample_text);
         tv.setText(stringFromJNI());
+
+
     }
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
     public native String stringFromJNI();
+
+
+//    public void play(View view){
+//        musicPlay.play();
+//    }
+//
+//    public void stop(View view){
+//        musicPlay.stop();
+//    }
 }
