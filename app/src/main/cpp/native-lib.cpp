e4ae0a24c89c65ade2095334115ea728a83bc57c
zyp <zyp19901003@163.com>
2018-10-15 18:29:07 +0800

    Temp commit

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -55,10 +55,47 @@ double division(int a, int b) {
 //    }
 //};
 
+template <class T>
 
+class Stack {
+private:
+    vector<T> elems;     // 元素
 
+public:
+    void push(T t);  // 入栈
+    void pop();               // 出栈
+    T top() const;            // 返回栈顶元素
+    bool empty() const{       // 如果为空则返回真。
+        return elems.empty();
+    }
+};
 
+template <class T>
+void Stack<T>::push (T  elem)
+{
+    // 追加传入元素的副本
+    elems.push_back(elem);
+}
 
+template <class T>
+void Stack<T>::pop ()
+{
+    if (elems.empty()) {
+        throw out_of_range("Stack<>::pop(): empty stack");
+    }
+    // 删除最后一个元素
+    elems.pop_back();
+}
+
+template <class T>
+T Stack<T>::top () const
+{
+    if (elems.empty()) {
+        throw out_of_range("Stack<>::top(): empty stack");
+    }
+    // 返回最后一个元素的副本
+    return elems.back();
+}
 
 class MyException {
 public:
@@ -217,6 +254,26 @@ Java_com_adasplus_update_c_MainActivity_text(JNIEnv *env, jobject instance) {
     string s3 = Max(s1, s2);
     LOGE("%s", s3.c_str());
 
+
+    try {
+        Stack<int>         intStack;  // int 类型的栈
+        Stack<string> stringStack;    // string 类型的栈
+
+
+        int a ;
+        intStack.push(a);
+
+        LOGE("%d", intStack.top());
+        stringStack.push("hello");
+        string temp = stringStack.top();
+        LOGE("%s", temp.c_str());
+        stringStack.pop();
+        stringStack.pop();
+    }
+    catch (exception const& ex) {
+        LOGE("%s", ex.what());
+        return -1;
+    }
 
     return 0;
 }
